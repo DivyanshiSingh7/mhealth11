33ed704d0e2067348ad9727d1139d67f64e0b1f1
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-06-18 22:25:06 +0530

    Updates

---
+++ precache-manifest.c0b8729d462bb0e700fa8a5270d96a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6783f1ae975c10e548aa68e14901fb",
+    "url": "/mhealth11/index.html"
+  },
+  {
+    "revision": "df5e5bb005b75f9179d5",
+    "url": "/mhealth11/static/css/main.d5bd4a3a.chunk.css"
+  },
+  {
+    "revision": "0b4b8056b0c4a47c6c6c",
+    "url": "/mhealth11/static/js/2.5885bdd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mhealth11/static/js/2.5885bdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5e5bb005b75f9179d5",
+    "url": "/mhealth11/static/js/main.9eb664bb.chunk.js"
+  },
+  {
+    "revision": "f396d25d16cea49ce870",
+    "url": "/mhealth11/static/js/runtime-main.58b6e49a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mhealth11/static/media/logo.5d5d9eef.svg"
+  }
+]);
